ef91cfaa3c7543a2e52fdf95224a2615251f0342
ahmed-shoushaa <ahmed.adel1199@yahoo.com>
2024-09-27 20:37:13 +0300

    add remote backend

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,5 +1,15 @@
+terraform {
+  backend "s3" {
+    bucket                  = "test-demo-terraform-bucket"
+    key                     = "state/eks.tfstate"
+    region                  = var.aws_region
+    access_key              = var.aws_access_key
+    secret_key              = var.aws_secret_key
+  }
+}
+
 provider "aws" {
-  region     = "us-east-1"
-  access_key = "AKIA6D6JBOQDZGFZLHPH"
-  secret_key = ""
+  region     = var.aws_region
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,4 +1,14 @@
 ##########################
+# Provider
+##########################
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+variable "aws_region" {
+  default = "us-east-1"
+}
+
+
+##########################
 # Main VPC 
 ##########################
 variable "vpc_cidr" {}
