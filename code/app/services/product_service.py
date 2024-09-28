6b1f1d43b7650b1ae44d620f8b32a332ae335187
ahmed-shoushaa <ahmed.adel1199@yahoo.com>
2024-09-28 14:45:53 +0300

    trigger pipeline

--- code/app/services/product_service.py
+++ code/app/services/product_service.py
@@ -3,7 +3,7 @@ class ProductService:
         self.products = [
             {"id": 1, "name": "Laptop"},
             {"id": 2, "name": "Smartphone"},
-            {"id": 3, "name": "Pipeline"},
+            {"id": 3, "name": "Computer"},
             {"id": 4, "name": "tv"}
         ]
 
